cef03c575e35ca65b7de380968b555d941007cd2
Valeriia <valeriiaholotiuk@gmail.com>
2025-06-02 13:29:38 -0230

    Initial commit with MyPoint.java

---
+++ MyPoint.java
@@ -0,0 +1,60 @@
+// This class represents a point in 2D space with x and y coordinates.
+
+public class MyPoint {
+    private int x;
+    private int y;
+
+    // Constructors
+    public MyPoint() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    // Setters
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // toString
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    // Method to calculate distance to another point
+    public double distance(int x, int y) {
+        int dx = this.x - x;
+        int dy = this.y - y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public double distance(MyPoint another) {
+        return distance(another.x, another.y);
+    }
+
+    public double distance() {
+        return Math.sqrt(x * x + y * y);
+    }
+}
